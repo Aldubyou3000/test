724401307948d12c42d01472c331044d46f55e07
Aldubyou3000 <132148373+Aldubyou3000@users.noreply.github.com>
2025-07-21 21:39:48 +0800

    script.lua

---
+++ script.lua
@@ -0,0 +1,4 @@
+local TeleportService = game:GetService("TeleportService")
+local placeId = game.PlaceId
+wait(10)  -- Adjust the delay as needed, e.g., 10 seconds
+TeleportService:Teleport(placeId)
