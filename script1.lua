45c6983901019bf83d06f372e915a2a8a9ba4e2a
Aldubyou3000 <132148373+Aldubyou3000@users.noreply.github.com>
2025-07-22 00:03:07 +0800

    Update script1.lua

--- script1.lua
+++ script1.lua
@@ -1,5 +1,5 @@
 local TeleportService = game:GetService("TeleportService")
 local player = game.Players.LocalPlayer
 
-wait(2)  -- Wait for 5 seconds to allow the game to load
+wait(1)  -- Wait for 5 seconds to allow the game to load
 TeleportService:Teleport(game.PlaceId, player)
